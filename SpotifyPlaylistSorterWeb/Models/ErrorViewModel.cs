1746f8d0a5cd70fdfb01cd92a2cc930cb04a4a93
Ben Whitfield <bpwhitfield@outlook.com>
2024-06-16 13:01:13 +0100

    Create project

---
+++ SpotifyPlaylistSorterWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SpotifyPlaylistSorterWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
