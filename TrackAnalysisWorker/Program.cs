83a3f0e9bb994310321e554e000d4ab8cb1656b0
Ben Whitfield <bpwhitfield@outlook.com>
2025-04-07 18:49:30 +0100

    Register Cyanite Client

--- TrackAnalysisWorker/Program.cs
+++ TrackAnalysisWorker/Program.cs
@@ -1,7 +1,16 @@
 using TrackAnalysisWorker;
+using TrackAnalysisWorker.Clients.Implementations;
+using TrackAnalysisWorker.Clients.Interfaces;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddHttpClient<ICyaniteClient, CyaniteClient>(client =>
+{
+    client.BaseAddress = new Uri("https://api.cyanite.ai/graphql");
+    client.DefaultRequestHeaders.Authorization = 
+        new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", builder.Configuration.GetValue<string>("Cyanite:AccessToken"));
+});
 
 var host = builder.Build();
 host.Run();
