0f7105cb8ddb863d115e5bec2dd0cab800a1132a
Ben Whitfield <bpwhitfield@outlook.com>
2025-06-24 20:03:38 +0100

    Create RabbitMQ consumer

--- TrackAnalysisWorker/Worker.cs
+++ TrackAnalysisWorker/Worker.cs
@@ -1,9 +1,14 @@
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using TrackAnalysisWorker.Services;
 
 namespace TrackAnalysisWorker;
 
 public class Worker : BackgroundService
 {
+    private IConnection _connection;
+    private IChannel _channel;
     private readonly ILogger<Worker> _logger;
     private readonly IAnalyserService _analyserService;
 
@@ -22,9 +27,31 @@ public class Worker : BackgroundService
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             }
 
-            _analyserService.Analyse("id");
+            var factory = new ConnectionFactory() { HostName = "localhost" }; // Or your actual host
+            _connection = await factory.CreateConnectionAsync();
+            _channel = await _connection.CreateChannelAsync();
 
-            await Task.Delay(1000, stoppingToken);
+            await _channel.QueueDeclareAsync(queue: "message",
+                                  durable: false,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+            consumer.ReceivedAsync += static async (sender, eventArgs) =>
+            {
+                byte[] body = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received: {message}");
+
+                await ((AsyncDefaultBasicConsumer)sender).Channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+            };
+
+            await _channel.BasicConsumeAsync(queue: "message",
+                                  autoAck: false,
+                                  consumer: consumer);
+
+            _analyserService.Analyse("id");
         }
     }
 }
